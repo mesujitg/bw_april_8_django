030adcaaf4d100223f127fb3b57a34f63e60600e
mesujitg <mesujitg@gmail.com>
2022-06-30 09:50:21 +0545

    selection in django

--- my_project/information/views.py
+++ my_project/information/views.py
@@ -1,17 +1,25 @@
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from applications.models import Application
 from information.models import Information
+from job_portal.settings import MEDIA_URL
 from jobs.models import Job
+from jobseekers.models import JobSeeker
 from organizations.models import Organization
 
 
 def show_home(request):
     org = Organization.objects.all()
-    jobs = Job.objects.count()
-    return render(request, 'index.html')
+    job = Job.objects.count()
+    candidates = JobSeeker.objects.count()
+    filled = Application.objects.filter(status='Selected').count()
+    stats = [candidates, job, filled, len(org)]
+    jobs = Job.objects.order_by('-id')[:10]
+    return render(request, 'index.html', {'organizations': org, 'stats': stats, 'jobs': jobs, 'MEDIA_URL': MEDIA_URL})
 
 
 def register(request):
@@ -20,7 +28,7 @@ def register(request):
         last_name = request.POST['ln']
         email = request.POST['em']
         username = request.POST['un']
-        password = request.POST['pw']
+        password = make_password(request.POST['pw'])
 
         user = User(first_name=first_name, last_name=last_name, email=email,
                     username=username, password=password)
@@ -59,12 +67,11 @@ def profile(request):
     # if not request.user.is_authenticated:
     #     messages.error(request, 'Invalid Access')
     #     return redirect('home')
-
     if request.method == 'POST':
         first_name = request.POST['fn']
         last_name = request.POST['ln']
         email = request.POST['em']
-        username = request.POST['un']
+        # username = request.POST['un']
         password = request.POST['pw']
 
         # job = Job.objects.get(id=jid)
@@ -72,8 +79,9 @@ def profile(request):
         user.first_name = first_name
         user.last_name = last_name
         user.email = email
-        user.username = username
-        user.password = password
+        # user.username = username
+        if request.user.password != password:
+            user.password = make_password(password)
         user.save()
 
         messages.success(request, 'Profile Updated')
